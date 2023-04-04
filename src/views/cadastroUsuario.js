ddecd367a4622af082c5e861128deb00af9a37c8
Bruno Mendes <wolfteam1910@gmail.com>
2023-04-04 11:05:35 -0300

    criando componente de combo box e view de cons~ulta lancamento e adicionando primereact no projeto

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Card from '../components/card'
 import FormGroup from '../components/form-group';
 import { withRouter } from 'react-router-dom';
-
+import UsuarioService from '../app/service/usuarioService';
+import { mensagemSucesso, mensagemErro } from '../components/toastr'
 class CadastroUsuario extends React.Component {
 
     state = {
@@ -12,8 +13,53 @@ class CadastroUsuario extends React.Component {
         senhaRepeticao: ''
     }
 
+    constructor() {
+        super();
+        this.service = new UsuarioService();
+    }
+
+    validar() {
+        const msgs = []
+
+        if (!this.state.nome) {
+            msgs.push('O campo Nome é obrigatório.');
+        }
+        if (!this.state.email) {
+            msgs.push('O campo Email é obrigatório');
+        } else if (!this.state.email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+            msgs.push('Informe um Email válido.');
+        }
+
+        if (!this.state.senha || !this.state.senhaRepeticao) {
+            msgs.push('Digite a senha 2x');
+        } else if (this.state.senha !== this.state.senhaRepeticao) {
+            msgs.push('As senhas não são iguais.')
+        }
+
+        return msgs;
+    }
+
     cadastrar = () => {
-        console.log(this.state)
+        const msgs = this.validar();
+        
+        if(msgs && msgs.length > 0){
+            msgs.forEach( (msg, index) => {
+                mensagemErro(msg)
+            });
+            return false;
+        }
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        this.service.salvar(usuario)
+            .then(response => {
+                mensagemSucesso('Usuário cadastrado com sucesso! Faça o login para acessar o sistema.')
+                this.props.history.push('/login')
+            }).catch(error => {
+                mensagemErro(error.response.data)
+            })
     }
 
     cancelar = () => {
