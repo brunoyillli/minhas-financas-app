2af403fa252c64014b037bb4c3fe55a92cd117aa
Bruno Mendes <wolfteam1910@gmail.com>
2023-04-22 17:52:07 -0300

    adicionando profile, correcao salvar usuario e ladingpage

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -5,12 +5,13 @@ import CadastroUsuario from "../views/cadastroUsuario";
 import Home from '../views/home'
 import ConsultaLancamentos from "../views/lancamentos/consulta-lancamentos";
 import CadastroLancamentos from "../views/lancamentos/cadastro-lancamentos";
+import LadingPage from "../views/ladingPage";
 import { AuthConsumer } from "./provedorAutenticacao";
 
 
 function RotaAutenticada({ component: Component, isUsuarioAutenticado, ...props }) {
     return (
-        <Route {...props} render={(componentProps) => {
+        <Route exact {...props} render={(componentProps) => {
             if (isUsuarioAutenticado) {
                 return (
                     <Component {...componentProps} />
@@ -28,9 +29,10 @@ function Rotas(props) {
     return (
         <HashRouter>
             <Switch>
+                <Route exact path="/" component={LadingPage} />
                 <RotaAutenticada isUsuarioAutenticado={props.isUsuarioAutenticado} path="/home" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/cadastro-usuarios" component={CadastroUsuario} />
                 <RotaAutenticada isUsuarioAutenticado={props.isUsuarioAutenticado} path="/consulta-lancamentos" component={ConsultaLancamentos} />
                 <RotaAutenticada isUsuarioAutenticado={props.isUsuarioAutenticado} path="/cadastro-lancamentos/:id?" component={CadastroLancamentos} />
             </Switch>
